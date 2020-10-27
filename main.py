3997350c401c071c2c420cb6fc26d76403316f39
Furri777 <70285562+Furri777@users.noreply.github.com>
2020-10-27 18:23:02 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ from Cube import RubiksCube
 cube = RubiksCube
 cube.scramble()
 
+def solve():
+    daisy()
+    white_cross()
+    white_corners()
+    second_layer()
+    
 def out_sides():
     """Debugging. Print out each face"""
     
@@ -192,7 +198,7 @@ def daisy():
             if y == 0: # Top edge
                 while 1:
                     if occupied_yellows[face] == False:
-                        #FBR'B'
+                        #FDR'D'
                         right = face_names.index(face) + 1
                         if right > 3:
                             right = 0
@@ -208,7 +214,7 @@ def daisy():
                         perm_turn_yellow()
 
             elif y == 2:
-                #F'BR'B'
+                #F'DR'D'
                 right = face_names.index(face) + 1
                 if right > 3:
                     right = 0
@@ -244,7 +250,7 @@ def daisy():
                         
     
 def white_cross():
-    
+    """Move the white edges from yellow to the correct spots on white"""
     def move_up(colour):
         names = {'r': 'red',
                  'b': 'blue',
@@ -275,5 +281,255 @@ def white_cross():
     move_up('o')
     move_up('g')
 
-daisy()
-white_cross()
+
+def white_corners():
+    current = []
+    rotation = []
+    def update_sides():
+        nonlocal current, rotation
+        current = []
+        rotation = []
+        wrb = (cube.white[2][2], cube.red[0][2], cube.blue[0][0])
+        wrg = (cube.white[2][0], cube.red[0][0], cube.green[0][2])
+        wob = (cube.white[0][2], cube.orange[0][0], cube.blue[0][2])
+        wog = (cube.white[0][0], cube.orange[0][2], cube.green[0][0])
+        yrb = (cube.yellow[0][2], cube.red[2][2], cube.blue[2][0])
+        yrg = (cube.yellow[0][0], cube.red[2][0], cube.green[2][2])
+        yob = (cube.yellow[2][2], cube.orange[2][0], cube.blue[2][2])
+        yog = (cube.yellow[2][0], cube.orange[2][2], cube.green[2][0])
+        corners  = [wrb, wrg, wob, wog, yrb, yrg, yob, yog]
+    
+        for x in range(8):
+            for i in range(3):
+                if corners[x][i] == 'w':
+                    current.append(x)
+                    break
+    update_sides()
+    def move_out(corner):
+        can_move = True
+        while 1:
+            for x in range(4):
+                if corner + 4 == current[x]:
+                    can_move == False
+            if can_move == True:
+                break
+            else:
+                cube.rotate_side_clockwise('yellow')
+                for x in range(4):
+                    if current[x] == 4:
+                        current[x] == 6
+                    elif current[x] == 5:
+                        current[x] = 4
+                    elif current[x] == 6:
+                        current[x] = 7
+                    elif current[x] == 7:
+                        current[x] = 5
+        # To move down we place the white corner in the top right
+        # And do R'D'R
+        def move(right):
+            cube.turn_side_anti_clockwise(right)
+            cube.turn_side_anti_clockwise('yellow')
+            cube.turn_side_clockwise(right)
+
+        if corner == 0:
+            move('blue')
+        elif corner == 1:
+            move('red')
+        elif corner == 2:
+            move('orange')
+        elif corner == 3:
+            move('green')
+        else:
+            return True
+            
+
+    while 1:
+        count = 0
+        for x in range(4):
+            if current[x] < 4:
+                while 1:
+                    if current[x]+4 in current:
+                        cube.turn_side_clockwise('yellow')
+                        update_sides()
+                    else:
+                        break
+                move_out(current[x])
+                count += 1
+                update_sides()
+        if count == 0:
+            break
+    
+    def move_up(front, side):
+        """Move a white from the bottom to where it needs to be"""
+        # If bottom right FDF'
+        # If bottom left F'D'F
+        # If yellow, put on right F'DFDDF'D'F
+        if side == 'right':
+            cube.turn_side_clockwise(front)
+            cube.turn_side_clockwise('yellow')
+            cube.turn_side_anti_clockwise(front)
+        elif side == 'left':
+            cube.turn_side_anti_clockwise(front)
+            cube.turn_side_anti_clockwise('yellow')
+            cube.turn_side_clockwise(front)
+        elif side == 'bottom':
+            cube.turn_side_anti_clockwise(front)
+            cube.turn_side_clockwise('yellow')
+            cube.turn_side_clockwise(front)
+            cube.turn_side_clockwise('yellow')
+            cube.turn_side_clockwise('yellow')
+            cube.turn_side_anti_clockwise(front)
+            cube.turn_side_anti_clockwise('yellow')
+            cube.turn_side_clockwise(front)
+
+    # Now all the corners should either be on the bottom
+    
+    while not(cube.red[0][2] == 'r' and cube.blue[0][0] == 'b'):
+        if (cube.red[2][2] in ['r','b','w'] and
+            cube.blue[2][0] in ['r','b','w'] and
+            cube.yellow[0][2] in ['r','b','w']):
+            if cube.red[2][2] == 'w':
+                move_up('red', 'right')
+            elif cube.blue[2][0] == 'w':
+                move_up('blue', 'left')
+            elif cube.yellow[0][2] == 'w':
+                move_up('blue', 'bottom')
+        else:
+            cube.turn_side_clockwise('yellow')
+    
+    while not(cube.red[0][0] == 'r' and cube.green[0][2] == 'g'):
+        if (cube.red[2][0] in ['r','g','w'] and
+            cube.green[2][2] in ['r','g','w'] and
+            cube.yellow[0][0] in ['r','g','w']):
+            if cube.red[2][0] == 'w':
+                move_up('red', 'left')
+            elif cube.green[2][2] == 'w':
+                move_up('green', 'right')
+            else:
+                move_up('red', 'bottom')
+        else:
+            cube.turn_side_clockwise('yellow')
+    
+    while not(cube.orange[0][0] == 'o' and cube.blue[0][2] == 'b'):
+        if (cube.orange[2][0] in ['o','b','w'] and
+            cube.blue[2][2] in ['o','b','w'] and
+            cube.yellow[2][2] in ['o','b','w']):
+            if cube.orange[2][0] == 'w':
+                move_up('orange', 'left')
+            elif cube.blue[2][2] == 'w':
+                move_up('blue', 'right')
+            else:
+                move_up('orange', 'bottom')
+        else:
+            cube.turn_side_clockwise('yellow')
+    
+    while not(cube.orange[0][2] == 'o' and cube.green[0][0] == 'g'):
+        if (cube.orange[2][2] in ['o','g','w'] and
+            cube.green[2][0] in ['o','g','w'] and
+            cube.yellow[2][0] in ['o','g','w']):
+            if cube.orange[2][2] == 'w':
+                move_up('orange', 'right')
+            elif cube.green[2][0] == 'w':
+                move_up('green', 'left')
+            else:
+                move_up('green', 'bottom')
+        else:
+            cube.turn_side_clockwise('yellow')
+
+def second_layer():
+    """Solve the middle layer"""
+    # To move from top to right do URU'R'U'F'UF
+    # To move from top to left do U'L'ULUFU'F'
+    def to_right(front):
+        sides = ['red', 'blue,' 'orange', 'green', 'red']
+        right = sides[sides.index(front)+1]
+        cube.turn_side_clockwise('yellow')
+        cube.turn_side_clockwise(right)
+        cube.turn_side_anti_clockwise('yellow')
+        cube.turn_side_anti_clockwise(right)
+        cube.turn_side_anti_clockwise('yellow')
+        cube.turn_side_anti_clockwise(front)
+        cube.turn_side_clockwise(right)
+        cube.turn_side_clockwise(front)
+        update_corners()
+
+    def to_left(front):
+        sides = ['red', 'blue,' 'orange', 'green']
+        left = sides[sides.index(front)-1]
+        cube.turn_side_anti_clockwise('yellow')
+        cube.turn_side_anti_clockwise(left)
+        cube.turn_side_clockwise('yellow')
+        cube.turn_side_clockwise(left)
+        cube.turn_side_clockwise('yellow')
+        cube.turn_side_clockwise(front)
+        cube.turn_side_anti_clockwise('yellow')
+        cube.turn_side_anti_clockwise(front)
+        update_corners()
+        
+    locations = []
+    edges = []
+    def update_corners():
+        nonlocal locations, edges
+        yr = (cube.yellow[0][1], cube.red[2][1])
+        yb = (cube.yellow[1][2], cube.blue[2][1])
+        yo = (cube.yellow[2][1], cube.orange[2][1])
+        yg = (cube.yellow[1][0], cube.green[2][1])
+        rb = (cube.red[1][2], cube.blue[1][0])
+        ob = (cube.orange[1][0], cube.blue[1][2])
+        og = (cube.orange[1][2], cube.green[1][2])
+        rg = (cube.red[1][0], cube.green[1][0])
+        edges = [yr, yb, yo, yg, rb, ob, og, rg]
+                
+    update_corners()
+    # First deal with any that are in the right place but wrong way round
+    def flip_side(side):
+        cube.turn_side_anti_clockwise('yellow')
+        to_right(side)
+        ccube.turn_side_anti_clockwise('yellow')
+        cube.turn_side_anti_clockwise('yellow')
+        to_right(side)
+        
+    if cube.red[1][2] == 'b' and cube.blue[1][0] == 'r':
+        flip_side('blue')
+    elif cube.blue[1][2] == 'o' and cube.orange[1][0] == 'b':
+        flip_side('orange')
+    elif cube.orange[1][2] == 'g' and cube.green[1][0] == 'o':
+        flip_side('green')
+    elif cube.green[1][2] == 'r' and cube.red[1][0] == 'g':
+        flip_side('red')
+
+    while 1:
+        moved = False
+        if edges[0] == ['b','r']:
+            to_left('red')
+            moved = True
+        elif edges[0] == ['g','r']:
+            to_right('red')
+            moved = True
+
+        if edges[1] == ['r','b']:
+            to_right('blue')
+            moved = True
+        elif edges[1] == ['o','b']:
+            to_left('blue')
+            moved = True
+
+        if edges[2] == ['o','b']:
+            to_right('orange')
+            moved = True
+        elif edges[2] ==['o','g']:
+            to_left('orange')
+            moved = True
+
+        if edges[3] == ['o','g']:
+            to_right('green')
+            moved = True
+        elif edges[3] == ['r','g']:
+            to_left('green')
+            moved = True
+
+        if moved == False:
+            cube.turn_side_clockwise('yellow')
+            print('g')
+        
+solve()
