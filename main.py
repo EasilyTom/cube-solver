2d5e4af903a8e1025c0538bde12c22bae6981573
Furri777 <70285562+Furri777@users.noreply.github.com>
2021-01-11 19:59:58 +0000

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from solver import solve
-import cube
-import gui
-
-cube_model = gui.model
-cube_array = cube.RubiksCube(cube_model)
-
-cube_array.turn_side_clockwise('white')
-cube_array.turn_side_clockwise('red')
